bf39851d7d712890ccbcdb2490378814ffaf7382
Jason Bock <jason.r.bock@outlook.com>
2025-12-23 10:51:06 -0600

    Initial setup for Day 6

---
+++ src/AdventOfCode2025/SolutionDay6.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Immutable;
+using System.Numerics;
+
+namespace AdventOfCode2025.Day6;
+
+public static class SolutionDay6
+{
+	public static BigInteger RunPart1(ImmutableArray<string> worksheet)
+	{
+		var answer = BigInteger.Zero;
+
+		return answer;
+	}
+}
